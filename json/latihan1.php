7dd08ffadb202f0eb6c07164c76f26d0b5bcbd77
fanideswi <fanitambunan03@gmail.com>
2025-05-28 19:35:27 +0700

    first commit

---
+++ json/latihan1.php
@@ -0,0 +1,25 @@
+<?php 
+
+// $mahasiswa = [
+//     [
+//         "nama" => "Fani Deswi Sapitri Tambunan",
+//         "nim" => "2217020011",
+//         "email" => "fanitambunan03@gmail.com"
+//     ],
+//     [
+//         "nama" => "Arfansyah",
+//         "nim" => "2217020012",
+//         "email" => "Arfansyah@gmail.com"
+//     ]
+// ];
+
+
+$dbh = new PDO('mysql:host=localhost;dbname=db_mahasiswa', 'root','root');
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
+
+ $data = json_encode($mahasiswa);
+ echo $data;
+
+?>
---
+++ json/script.js
@@ -0,0 +1,24 @@
+// KALAU OBJEKNYA JADI JSON
+// let mahasiswa = {
+//     nama : "fani tambunan",
+//     nim : "2217020011",
+//     email : "fanitambunan03@gmail.com"
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+// JSON KE OBJEK
+// let xhr = new XMLHttpRequest();
+// xhr.onreadystatechange = function() {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//        let mahasiswa = JSON.parse(this.responseText);
+//        console.log(mahasiswa);
+//     }
+// }
+// xhr.open('GET','coba.json',true);
+// xhr.send();
+
+$.getJSON('coba.json', function(data) {
+    console.log(data);
+
+});
